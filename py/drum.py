ce635b59221bb9c6c6f3e4b6282d579718b5acb2
Sam Root <sjroot@gmail.com>
2023-02-07 16:56:10 -0700

    slither

--- py/drum.py
+++ py/drum.py
@@ -1,6 +1,6 @@
 def card(deg):
     with open('cards/drum.txt','w') as drumcards:
-        drumcards.write(f'% Control drum absorber pads, numbered clockwise from 9 o’clock\n')
+        drumcards.write(f'% Control drum absorber pads, numbered clockwise from 9 o clock\n')
         drumcards.write(f'surf 80 pad -45.05 0 16 17 {deg-45} {deg+45} % drum 1\n')
         drumcards.write(f'surf 81 pad -31.8551605 31.8551605 16 17 {deg-0} {deg+90} % drum 2\n')
         drumcards.write(f'surf 82 pad 0 45.05 16 17 {deg+45} {deg+135} % drum 3\n')
